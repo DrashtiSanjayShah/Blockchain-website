42d94df3ca2bee65df961f04e36212885de6fa97
DrashtiS <94853646+DrashtiSanjayShah@users.noreply.github.com>
2023-11-15 19:40:57 +0530

    Delete src directory

--- src/utils/shortenAddress.js
+++
@@ -1,8 +0,0 @@
-export const shortenAddress = (address) => {
-    if (!address) {
-      return "Invalid Address";
-    }
-  
-    return `${address.slice(0, 5)}...${address.slice(address.length - 4)}`;
-  };
-  
